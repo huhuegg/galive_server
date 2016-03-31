cb3bae3f33d6d2cabacbd1abeb4f59619110ce2e
lugq1001 <lugq1001@icloud.com>
2016-03-31 23:37:15 +0800

    no message

--- galive_server_logic/src/main/java/com/galive/logic/handler/LogicHandler.java
+++ galive_server_logic/src/main/java/com/galive/logic/handler/LogicHandler.java
@@ -4,14 +4,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * 自定义@LogicHandler标签 用于处理Websocket业务逻辑
+ * 自定义@LogicHandler标签 用于处理业务逻辑
  * @author Luguangqing
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogicHandler {
 
-	public String commandID(); // MessageID 请求id
+	public String command(); // command 请求命令
 	public String desc(); // 文字说明  无业务用途
 
 }
--- galive_server_logic/src/main/java/com/galive/logic/helper/AnnotationManager.java
+++ galive_server_logic/src/main/java/com/galive/logic/helper/AnnotationManager.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import com.galive.common.utils.PackageUtil;
 import com.galive.logic.handler.BaseHandler;
+import com.galive.logic.handler.LogicHandler;
 
 /**
  * 自定义标签工具类 
@@ -29,23 +30,10 @@ public class AnnotationManager {
 		logger.info("加载@LogicHandler");
 		List<Class<?>> classes = PackageUtil.getClasssFromPackage(packageName);
 		for (Class<?> clazz : classes) {
-			BaseHandler handler = clazz.getAnnotation(BaseHandler.class);
-			if (handler != null) {
-				logger.info(clazz.getName() + ":" + handler.desc() + "(" + handler.id() + ")");
-				BaseHandler.put(handler.id(), clazz);
-			}
-		}
-		logger.info("@ServletHandler加载完成");
-		
-		packageName = com.galive.logic.BaseHandler.ws.handler.BaseHandler.class.getPackage().getName();
-		logger.info("扫描包：" + packageName);
-		logger.info("加载@WebSocketHandler");
-		classes = PackageUtil.getClasssFromPackage(packageName);
-		for (Class<?> clazz : classes) {
 			LogicHandler handler = clazz.getAnnotation(LogicHandler.class);
 			if (handler != null) {
-				logger.info(clazz.getName() + ":" + handler.desc() + "(" + handler.commandID() + ")");
-				wsHandlers.put(handler.commandID(), clazz);
+				logger.info(clazz.getName() + ":" + handler.desc() + "(" + handler.command() + ")");
+				logicHandlers.put(handler.command(), clazz);
 			}
 		}
 		logger.info("@LogicHandler加载完成");
@@ -56,9 +44,9 @@ public class AnnotationManager {
 	 * @param messageID
 	 * @return
 	 */
-	public static BaseHandler createServletHandlerInstance(String messageID) {
+	public static BaseHandler createLogicHandlerInstance(String messageID) {
 		BaseHandler handler = null;
-		Class<?> clazz = servletHandlers.get(messageID);
+		Class<?> clazz = logicHandlers.get(messageID);
 		if (clazz != null) {
 			try {
 				handler = (BaseHandler) clazz.newInstance();
